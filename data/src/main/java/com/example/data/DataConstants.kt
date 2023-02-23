c9b5db38405d22853345c44188dfd5c96f5d525a
ninidze1 <giorgi.ninidze.1@btu.edu.ge>
2023-02-23 21:11:50 +0400

    Started Modularizing And Refactoring Code

---
+++ data/src/main/java/com/example/data/DataConstants.kt
@@ -0,0 +1,13 @@
+package com.example.data
+
+object DataConstants {
+    const val SESSION_PREF = "session_pref"
+    const val SESSION = "session"
+    const val NETWORK_PAGE_SIZE = 7
+    const val STARTING_PAGE_INDEX = 1
+    const val LANG_ENG = "en-US"
+    const val PASS_KEY = "passcode"
+    const val UNAUTHORISED = "not_registered"
+
+    const val API_KEY = "8a7a052285633fc4c44c60d8d3f24984"
+}
